cd81c615fb717ccf0d9a7817a367f38c1f05ab0b
Alexandr Timoshenko <thick.tav@gmail.com>
2017-09-22 01:26:55 +0200

    7

--- app/src/main/java/com/tunebrains/aarsample/MainActivity.java
+++ app/src/main/java/com/tunebrains/aarsample/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //com.anchorfree.hdr.AFHydra.NativeX();
+        com.anchorfree.hdr.AFHydra.NativeX();
     }
 }
